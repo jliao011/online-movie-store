bd4a9e204a1e8bf0b4dfc9711f467e658e479395
LATTES <liaojiafeng93@gmail.com>
2017-04-11 14:33:01 -0500

    4/11

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,14 @@
 $("document").ready(function(){
+	
+	$("#loginBtn").click(function(){
+		$("#loginModal").css("display","block");
+	});
+
+	$("#loginModal span.close").click(function(){
+		$("#loginModal").css("display","none");
+	});
+
+
 
 
 
