ed9f4db630594f596e4e9a082f05690b23eea196
Nicole_S <shibiyun2013@163.com>
2017-04-11 15:10:43 -0500

    Add files via upload

---
+++ register.php
@@ -0,0 +1,33 @@
+<?php
+	session_start();
+	$email = $username = $password = "";
+	$err = "User already exists";
+	
+	$conn = new mysqli('localhost', 'root', 'root', 'Movie_store');
+	
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		$email = text_input($_POST["user_email"]);
+		$username = text_input($_POST["user_name"]);
+		$password = text_input($_POST['password']);
+	}
+	
+	$query = 'SELECT * FROM `user` WHERE `user_name`="'.$username.'"';
+	$result = $conn -> query($query);
+	if(mysqli_num_rows($result) == 0){
+		$query = 'INSERT INTO `user`(`user_email`, `user_name`, `password`) VALUES ("'.$email.'","'.$username.'","'.$password.'")';
+		$result = $conn -> query($query);
+		$_SESSION['username'] = $username;
+		echo "Successfully login.";
+	}
+	else{
+		echo $err;
+	}
+		
+	function text_input($data)
+	{
+ 		$data = trim($data);
+  		$data = stripslashes($data);
+  		$data = htmlspecialchars($data);
+  		return $data;
+	}
+?>
