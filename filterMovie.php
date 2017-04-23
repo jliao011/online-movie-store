c0113d466f3e9fd70c7b4142a697ceac00c2e218
lattes <liaojiafeng93@gmail.com>
2017-04-23 04:22:02 -0500

    update filter
    Signed-off-by: lattes <liaojiafeng93@gmail.com>

---
+++ filterMovie.php
@@ -0,0 +1,79 @@
+<?php
+	session_start();
+	$err = "";
+	$list = array();
+	$mysql = mysqli_connect("localhost:8889","root","root","MOVIE_STORE");
+	if (mysqli_connect_errno()){
+		$err .= "Failed to connect to MySQL: ".mysqli_connect_error()."<br/>";
+	}
+
+	$movie_name = mysqli_real_escape_string($mysql,$_POST['movie_name']);
+	$category = $_POST['category'];
+
+
+	if($movie_name != ""){
+		$query = "SELECT * FROM MOVIE WHERE movie_name LIKE '%".$movie_name."%';";
+		$result = mysqli_query($mysql,$query);
+		$list = fetch_name($result);
+	}else{
+		if($category == ""){
+			$query = "SELECT * FROM MOVIE;";
+			$result = mysqli_query($mysql,$query);
+			$list = fetch_name($result);
+		}else{
+			$query = "SELECT * FROM CATEGORY WHERE category = '".$category."';";
+			$result = mysqli_query($mysql,$query);
+
+			while($pair = mysqli_fetch_array($result)){
+				$id = $pair['movie_id'];
+				$query = "SELECT * FROM MOVIE WHERE movie_id = $id;";
+
+				$tuple = mysqli_query($mysql,$query);
+				$movie = mysqli_fetch_array($tuple);
+				$name = $movie['movie_name'];
+				$rating = $movie['movie_rating'];
+				$year = $movie['year'];
+				$price = $movie['price'];
+				$cat_array = array();
+
+				$query = "SELECT * FROM CATEGORY WHERE movie_id = $id;";
+				$cat_result = mysqli_query($mysql,$query);
+				while($cat = mysqli_fetch_array($cat_result)){
+					$cat_temp = $cat['category'];
+					array_push($cat_array,$cat_temp);
+				}
+				$single_movie = array('id'=>$id,'name'=>$name,'rating'=>$rating,'year'=>$year,'price'=>$price,'category'=>$cat_array);
+				array_push($list,$single_movie);
+			}
+		}
+	}
+
+
+	function fetch_name($result){
+		$list = array();
+		while($movie = mysqli_fetch_array($result)){
+			$id = $movie['movie_id'];
+			$name = $movie['movie_name'];
+			$rating = $movie['movie_rating'];
+			$year = $movie['year'];
+			$price = $movie['price'];
+			$cat_array = array();
+
+			$query = "SELECT * FROM CATEGORY WHERE movie_id = $id;";
+			$cat_result = mysqli_query($mysql,$query);
+			while($cat = mysqli_fetch_array($cat_result)){
+				$cat_temp = $cat['category'];
+				array_push($cat_array,$cat_temp);
+			}
+			$single_movie = array('id'=>$id,'name'=>$name,'rating'=>$rating,'year'=>$year,'price'=>$price,'category'=>$cat_array);
+			array_push($list,$single_movie);
+		}
+		return $list;	
+	}
+
+
+
+	$message = array('err'=>$err,'list'=>$list);
+	echo json_encode($message);
+
+?>
--- scripts/filterMovie.js
+++ scripts/filterMovie.js
@@ -1,9 +1,51 @@
 $("document").ready(function() {
 
-
 	$("#filter-panel span.btn").click(function(){
-		var movie_name = $("#search-name").val();
+		var movie_name = $("#search-name").val().trim();
 		var category = $("#search-category").val();
+
+
+		$.ajax({
+			url:"filterMovie.php",
+			data: {"movie_name":movie_name,"category":category},
+			method: "post",
+			dataType: "json",
+			success: function(response){
+				$("div.home p.error").html(response.err);
+
+	
+				$(response.list).each(function(index,element){
+					var id = element.id;
+					var name = element.name;
+					var rating = element.rating;
+					var year = element.year;
+					var price = element.price;
+					var category = [];
+					$(element.category).each(function(index,element){
+						category.push(element);
+					});
+
+					
+					$("#listing").append(id+"  "+name+"  "+rating+"  "+year+"  "+price+"  "+category+"<br>");
+
+
+
+
+				});
+
+
+
+				
+
+			},
+			error: function(){
+				alert("Error: cannot link filterMovie.php.");
+			}
+
+
+		});
+
+
 		$("div.listMovie").text(movie_name + category);
 
 	});
